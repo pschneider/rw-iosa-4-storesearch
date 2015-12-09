b52e2aa391bb70ed2d10f649abc8d13bbe3deb3d
Patrick Schneider <me@patrickschneider.io>
2015-12-09 22:42:04 +0100

    Putting NSURLSession into action

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -51,14 +51,6 @@ class SearchViewController: UIViewController {
     }
 
     // MARK: Networking
-    func performStoreRequestWithURL(url: NSURL) -> String? {
-        do {
-            return try String(contentsOfURL: url, encoding: NSUTF8StringEncoding)
-        } catch {
-            print("Download Error: \(error)")
-            return nil
-        }
-    }
 
     func showNetworkError() {
         let alert = UIAlertController(title: "Whooops...", message: "There was an error reading from the iTunes Store. Please try again.", preferredStyle: .Alert)
@@ -76,9 +68,7 @@ class SearchViewController: UIViewController {
         return NSURL(string: urlString)!
     }
 
-    func parseJSON(jsonString: String) -> [String: AnyObject]? {
-        guard let data = jsonString.dataUsingEncoding(NSUTF8StringEncoding) else { return nil }
-
+    func parseJSON(data: NSData) -> [String: AnyObject]? {
         do {
             return try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String: AnyObject]
         } catch {
@@ -229,33 +219,34 @@ extension SearchViewController: UISearchBarDelegate {
             hasSearched = true
             searchResults = [SearchResult]()
 
-            let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
-
-            dispatch_async(queue) {
-                let url = self.urlWithSearchText(searchBar.text!)
-                print("URL: \(url)")
-
-                if let jsonString = self.performStoreRequestWithURL(url),
-                    let dictionary = self.parseJSON(jsonString) {
+            let url = urlWithSearchText(searchBar.text!)
+            let session = NSURLSession.sharedSession()
+            let dataTask = session.dataTaskWithURL(url) {
+                data, response, error in
+                if let error = error  {
+                    print("Failure! \(error)")
+                } else if let httpResponse = response as? NSHTTPURLResponse where httpResponse.statusCode == 200 {
+                    if let data = data, dictionary = self.parseJSON(data) {
                         self.searchResults = self.parseDictionary(dictionary)
-
-                        // searchResults.sortInPlace({ (result1, result2) -> Bool in
-                        //    return result1.name.localizedStandardCompare(result2.name) == .OrderedAscending
-                        // })
-                        // searchResults.sortInPlace {
-                        //     $0.name.localizedStandardCompare($1.name) == .OrderedAscending
-                        // }
                         self.searchResults.sortInPlace(<)
+
                         dispatch_async(dispatch_get_main_queue()) {
                             self.isLoading = false
                             self.tableView.reloadData()
                         }
                         return
+                    }
+                } else {
+                    print("Failure! \(response!)")
                 }
                 dispatch_async(dispatch_get_main_queue()) {
+                    self.hasSearched = false
+                    self.isLoading = false
+                    self.tableView.reloadData()
                     self.showNetworkError()
                 }
             }
+            dataTask.resume()
         }
     }
 
