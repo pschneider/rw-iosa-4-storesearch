58547fa453e683da23a210f4e3695ae7b10d068a
Patrick Schneider <me@patrickschneider.io>
2015-12-11 22:25:07 +0100

    Adding the rest of the controls

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -9,11 +9,32 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+
+    // MARK: Outlets
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var aristNameLabel: UILabel!
+    @IBOutlet weak var kindLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var priceButton: UIButton!
+
     // MARK: Life-Cycle
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         modalPresentationStyle = .Custom
         transitioningDelegate = self
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.tintColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
+        popupView.layer.cornerRadius = 10
+
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("close"))
+        gestureRecognizer.cancelsTouchesInView = false
+        gestureRecognizer.delegate = self
+        view.addGestureRecognizer(gestureRecognizer)
     }
 
     // MARK: Actions
@@ -22,6 +43,7 @@ class DetailViewController: UIViewController {
     }
 }
 
+// Change presentation mode
 extension DetailViewController: UIViewControllerTransitioningDelegate {
     func presentationControllerForPresentedViewController(
         presented: UIViewController,
@@ -32,5 +54,12 @@ extension DetailViewController: UIViewControllerTransitioningDelegate {
             presentedViewController: presented,
             presentingViewController: presenting
         )
+    }
+}
+
+// Dismiss by tapping outside of the popup view
+extension DetailViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldReceiveTouch touch: UITouch) -> Bool {
+        return (touch.view === self.view)
     }
 }
