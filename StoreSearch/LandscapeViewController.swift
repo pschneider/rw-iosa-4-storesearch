23c83834a7e9ddfb1be30b9918d2c1d41fb408ce
Patrick Schneider <me@patrickschneider.io>
2015-12-17 19:38:05 +0100

    Adding the scroll view

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -10,10 +10,39 @@ import UIKit
 
 class LandscapeViewController: UIViewController {
 
+    // MARK: Outlets
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // convert layout to auto layout constraints by UIKit
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+
+        scrollView.removeConstraints(scrollView.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+
+        scrollView.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+        scrollView.contentSize = CGSize(width: 1000, height: 1000)
+
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        scrollView.frame = view.bounds
+
+        pageControl.frame = CGRect(
+            x: 0,
+            y: view.frame.size.height - pageControl.frame.size.height,
+            width: view.frame.size.width,
+            height: pageControl.frame.size.height
+        )
     }
 
     override func didReceiveMemoryWarning() {
